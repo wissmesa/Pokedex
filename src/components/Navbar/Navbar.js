2b66c7a069452374220c2174dd125ed43d3fd903
Luis Mesa <luismesa818@gmail.com>
2021-04-14 22:58:05 +0200

    Segundo Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className="Navbar"  >
+            P O K E D E X
+            
+        </div>
+    );
+}
+
+export default Navbar;
